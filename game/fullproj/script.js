581926bdcca11bda57ebca135446b448ad152d45
Ben Trinh <bjtrinh@ucdavis.edu>
2025-03-11 12:07:50 -0700

    add

--- game/fullproj/script.js
+++ game/fullproj/script.js
@@ -9,6 +9,13 @@
     const p2Score = document.querySelector('#p2score');
     const actionArea = document.querySelector('#actions');
     const instructions = document.querySelector('#instructions');
+    const trackLength = document.querySelector("#characters").offsetWidth - 120;
+    let p1Position;
+    let p2Position; 
+    const p1Pig = document.querySelector("#p1character");
+    const p2Pig = document.querySelector("#p2character");
+    const diceSound = new Audio('sounds/dice.mp3');
+    const oink = new Audio('sounds/oink.mp3');
 
     const gameData = {
         dice:['1die.png', '2die.png', '3die.png', '4die.png', '5die.png', '6die.png'],
@@ -26,6 +33,7 @@
     startGame.addEventListener('click', function(){
         gameControl.innerHTML = '<h2>The game has started</h2>';
         gameControl.innerHTML += '<button id="quit">Wanna Quit?</button>';
+        // p1Pig.offsetWidth = `${trackLength}px`;
 
         document.querySelector('#quit').addEventListener('click', function(){
             location.reload();
@@ -35,10 +43,9 @@
     });
 
     function setUpTurn(){
-        instructions.innerHTML = `<p>Roll the dice for the ${gameData.players[gameData.index]}`;
+        instructions.innerHTML = `<p>Roll the dice for the <b>${gameData.players[gameData.index]}</b>`;
         actionArea.innerHTML = '<button id="roll">Roll the Dice</button>';
 
-        //change here
         gameData.resetPoint = gameData.score[gameData.index];
 
         document.querySelector('#roll').addEventListener('click', function(){
@@ -47,39 +54,96 @@
     }
 
     function throwDice(){
+        diceSound.play();
         actionArea.innerHTML = '';
         gameData.roll1 = Math.floor(Math.random() * 6) + 1;
         gameData.roll2 = Math.floor(Math.random() * 6) + 1;
-        instructions.innerHTML = `<p>Roll the dice for the ${gameData.players[gameData.index]}</p>`;
+        instructions.innerHTML = `<p>Roll the dice for the <b>${gameData.players[gameData.index]}</b></p>`;
         gameData.rollSum = gameData.roll1 + gameData.roll2;
         game.innerHTML = `<img src="images/${gameData.dice[gameData.roll1-1]}"> <img src="images/${gameData.dice[gameData.roll2-1]}">`;
         
 
         if(gameData.rollSum===2){
-            instructions.innerHTML += '<p>oh snap! snake eyes!</p>';
+            if (gameData.index == 0){
+                setTimeout(function(){
+                    p1Pig.className = 'spin2';
+                    oink.play();
+                }, 2);
+                p1Pig.className = '';
+            } else if (gameData.index == 1){
+                setTimeout(function(){
+                    p2Pig.className = 'spin2';
+                    oink.play();
+                }, 2);
+                p2Pig.className = '';
+            }
+
+            instructions.innerHTML += '<p>oh snap! <b>snake eyes</b>!</p>';
             gameData.score[gameData.index]=0;
             gameData.index ? (gameData.index=0) : (gameData.index =1);
             
             //show current score
             console.log(`p1: ${gameData.score[0]} p2: ${gameData.score[1]}`);
             showCurrentScore();
-            setTimeout(setUpTurn,2000);
+            setTimeout(setUpTurn,1000);
         }else if(gameData.roll1===1 || gameData.roll2===1){
             //change here
             gameData.score[gameData.index] = gameData.resetPoint;
 
+            if (gameData.index == 0){
+                setTimeout(function(){
+                    p1Pig.className = 'spin1';
+                    oink.play();
+                }, 2);
+                p1Pig.className = '';
+            } else if (gameData.index == 1){
+                setTimeout(function(){
+                    p2Pig.className = 'spin1';
+                    oink.play();
+                }, 2);
+                p2Pig.className = '';
+            }
+
             gameData.index ? (gameData.index=0) : (gameData.index =1);
-            instructions.innerHTML += `<p>sorry, one of your rolls was a one, switching to ${gameData.players[gameData.index]}</p>`
+            instructions.innerHTML += `<p>One of your rolls was a <b>one</b>, switching to ${gameData.players[gameData.index]}</p>`
 
             console.log(`p1: ${gameData.score[0]} p2: ${gameData.score[1]}`);
             showCurrentScore();
-            setTimeout(setUpTurn,2000);
+            setTimeout(setUpTurn,1000);
         }else{
             gameData.score[gameData.index] = gameData.score[gameData.index] + gameData.rollSum;
             console.log(`p1: ${gameData.score[0]} p2: ${gameData.score[1]}`);
 
-            instructions.innerHTML += `Your pig moved ${gameData.rollSum}m forward.`
+            instructions.innerHTML += `Your pig moved <b>${gameData.rollSum}m</b> forward.`
             actionArea.innerHTML = '<button id="rollagain">Roll again</button> or <button id="pass">Pass</button>';
+
+            if (gameData.roll1 == gameData.roll2){
+                if (gameData.index == 0){
+                    instructions.innerHTML += ` And you rolled a <b>double</b>. You sent the other pig <b>${gameData.roll1}m</b> back.`
+                    if (gameData.score[1] <= gameData.roll1){
+                        gameData.score[1] = 0;
+                        oink.play();
+                    } else{
+                        gameData.score[1] = gameData.score[1] - gameData.roll1;
+                    }
+                    setTimeout(function(){
+                        p2Pig.className = 'spin1';
+                    }, 2);
+                    p2Pig.className = '';
+                } else if (gameData.index == 1){
+                    instructions.innerHTML += ` And you rolled a <b>double</b>. You sent the other pig <b>${gameData.roll1}m</b> back.`
+                    if (gameData.score[0] <= gameData.roll1){
+                        gameData.score[0] = 0;
+                        oink.play();
+                    } else{
+                        gameData.score[0] = gameData.score[0] - gameData.roll1;
+                    }
+                    setTimeout(function(){
+                        p1Pig.className = 'spin1';
+                    }, 2);
+                    p1Pig.className = '';
+                }
+            }
 
             document.querySelector('#rollagain').addEventListener('click', function(){
                 //setUpTurn();
@@ -95,12 +159,19 @@
         }
     }
 
-    function checkWinningCondition() {
+    function checkWinningCondition(){
         if (gameData.score[gameData.index] > gameData.gameEnd) {
             scores.innerHTML = `<h2>${gameData.players[gameData.index]} wins!`;
+            scores.className = 'scorefinal';
     
             actionArea.innerHTML = '';
             document.querySelector('#quit').innerHTML = 'Start a New Game?';
+
+            if (gameData.index == 0){
+                p1Pig.style.left = `${trackLength}px`;
+            } else if (gameData.index == 1){
+                p2Pig.style.left = `${trackLength}px`;
+            }
         } else {
             showCurrentScore();
         }
@@ -109,7 +180,19 @@
     function showCurrentScore(){
         p1Score.innerHTML = `<p><strong>${gameData.players[0]}: ${gameData.score[0]}m</strong></p>`
         p2Score.innerHTML = `<p><strong>${gameData.players[1]}: ${gameData.score[1]}m</strong><p>`
+    
+        movePigs(); 
     }
-
-
+    
+    function movePigs(){
+        p1Position = (gameData.score[0]/gameData.gameEnd) * trackLength;
+        p2Position = (gameData.score[1]/gameData.gameEnd) * trackLength;
+        
+        // console.log(trackLength);
+        // console.log(p1Position);
+        // console.log(p2Position);
+    
+        p1Pig.style.left = `${p1Position}px`;
+        p2Pig.style.left = `${p2Position}px`;
+    }
 })();
